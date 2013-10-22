48621fce21d165569e403087dde1cfeac6bba3e6
yangning <yangning1123@gmail.com>
2013-10-22 21:02:52 +0800

    *** empty log message ***

--- setxbacklight_goagent.sh
+++ setxbacklight_goagent.sh
@@ -7,11 +7,11 @@ export PATH
 
 # 设置背光亮度
 
-#gnome3
-#xbacklight -set 70
+# gnome3
+xbacklight -set 70
 
-#nemo
-pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --set-brightness 7
+# nemo
+#pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --set-brightness 7
 
 # 使用goagent代理
 

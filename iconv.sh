ce41ab058142d7df973526f399333ea165a0936e
yangning <yangning1123@gmail.com>
2013-10-23 22:49:14 +0800

    编码转换，解决乱码

--- iconv.sh
+++ iconv.sh
@@ -3,8 +3,7 @@
 #       转换文件编码gb2312->utf-8,单个文件及批处理文件均可
 # History:
 # 2013/1/1          yangning       First Release
-
-# 2013/03/01	   yangning	  Second Release
+# 2013/03/01	    yangning	   Second Release
 #	增加对较大文件的转码支持
 export PATH
 

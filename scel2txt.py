fe9cc9531d547719b977d0401ffc93e9cff0899a
yangning <yangning1123@gmail.com>
2014-04-24 11:31:27 +0800

    搜狗词库转成txt

--- scel2txt.py
+++ scel2txt.py
@@ -55,27 +55,27 @@ def byte2str(data):
             ret += u'\n'
         elif t != u' ':
             ret += t
-            i += 2
-            return ret
-            #获取拼音表
-            def getPyTable(data):
+        i += 2
+    return ret
+#获取拼音表
+def getPyTable(data):
 
     if data[0:4] != "\x9D\x01\x00\x00":
         return None
-        data = data[4:]
-        pos = 0
-        length = len(data)
-        while pos < length:
-            index = struct.unpack('H',data[pos]+data[pos+1])[0]
-            #print index,
-            pos += 2
-            l = struct.unpack('H',data[pos]+data[pos+1])[0]
-            #print l,
-            pos += 2
-            py = byte2str(data[pos:pos+l])
-            #print py
-            GPy_Table[index]=py
-            pos += l
+    data = data[4:]
+    pos = 0
+    length = len(data)
+    while pos < length:
+        index = struct.unpack('H',data[pos]+data[pos+1])[0]
+        #print index,
+        pos += 2
+        l = struct.unpack('H',data[pos]+data[pos+1])[0]
+        #print l,
+        pos += 2
+        py = byte2str(data[pos:pos+l])
+        #print py
+        GPy_Table[index]=py
+        pos += l
 
 #获取一个词组的拼音
 def getWordPy(data):
@@ -87,7 +87,7 @@ def getWordPy(data):
         index = struct.unpack('H',data[pos]+data[pos+1])[0]
         ret += GPy_Table[index]
         pos += 2    
-        return ret
+    return ret
 
 #获取一个词组
 def getWord(data):
@@ -99,7 +99,7 @@ def getWord(data):
         index = struct.unpack('H',data[pos]+data[pos+1])[0]
         ret += GPy_Table[index]
         pos += 2    
-        return ret
+    return ret
 
 #读取中文表    
 def getChinese(data):
@@ -137,7 +137,7 @@ def getChinese(data):
 
             #保存
             GTable.append((count,py,word))
-            
+        
             #到下个词的偏移位置
             pos +=  ext_len
 
@@ -152,8 +152,8 @@ def deal(file_name):
     if data[0:12] !="\x40\x15\x00\x00\x44\x43\x53\x01\x01\x00\x00\x00":
         print "确认你选择的是搜狗(.scel)词库?"
         sys.exit(0)
-        #pdb.set_trace()
-        
+    #pdb.set_trace()
+    
     print "词库名：" ,byte2str(data[0x130:0x338])#.encode('GB18030')
     print "词库类型：" ,byte2str(data[0x338:0x540])#.encode('GB18030')
     print "描述信息：" ,byte2str(data[0x540:0xd40])#.encode('GB18030')
@@ -167,18 +167,18 @@ if __name__ == '__main__':
 
     #将要转换的词库添加在这里就可以了
     o = ['计算机词汇大全【官方推荐】.scel',
-         'IT计算机.scel',
-         '计算机词汇大全【官方推荐】.scel',
-         '北京市城市信息精选.scel',
-         '常用餐饮词汇.scel',
-         '成语.scel',
-         '成语俗语【官方推荐】.scel',
-         '法律词汇大全【官方推荐】.scel',
-         '房地产词汇大全【官方推荐】.scel',
-         '手机词汇大全【官方推荐】.scel',
-         '网络流行新词【官方推荐】.scel',
-         '歇后语集锦【官方推荐】.scel',
-         '饮食大全【官方推荐】.scel',
+    'IT计算机.scel',
+    '计算机词汇大全【官方推荐】.scel',
+    '北京市城市信息精选.scel',
+    '常用餐饮词汇.scel',
+    '成语.scel',
+    '成语俗语【官方推荐】.scel',
+    '法律词汇大全【官方推荐】.scel',
+    '房地产词汇大全【官方推荐】.scel',
+    '手机词汇大全【官方推荐】.scel',
+    '网络流行新词【官方推荐】.scel',
+    '歇后语集锦【官方推荐】.scel',
+    '饮食大全【官方推荐】.scel',
     ]
     
     for f in o:
@@ -191,4 +191,5 @@ if __name__ == '__main__':
         #我没排序，所以结果是按照上面输入文件的顺序
         f.write( unicode('{%(count)s}' %{'count':count}+py+' '+ word).encode('GB18030') )#最终保存文件的编码，可以自给改
         f.write('\n')
-        f.close()    
+    f.close()    
+
